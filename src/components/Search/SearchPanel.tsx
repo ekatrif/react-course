443b8677083f0ea1e3c2a38ce564fc919e666d01
Ekatrif <katiapervak@gmail.com>
2023-10-31 21:35:46 +0300

    fix: save search term on click button

--- src/components/Search/SearchPanel.tsx
+++ src/components/Search/SearchPanel.tsx
@@ -26,6 +26,7 @@ class SearchPanel extends Component<IProps> {
   handleClick() {
     const { startSearch } = this.props;
     startSearch();
+    this.setSearchText();
   }
 
   setSearchText = () => {
